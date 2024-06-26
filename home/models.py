5f8bdf21de83a93d9330b5e14c0aeb03368262f7
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2024-06-26 22:10:16 +0300

    add image and details page and fix bugs with modifications on book

--- home/models.py
+++ home/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 
+def imagesave(instance, filename):
+    extension = filename.split(".")[-1]
+    return "books/%s/%s.%s"%("images", instance.name.replace(' ', '_'), extension)
+
+
+
 class Author(models.Model):
     name            = models.CharField(max_length=150)
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     name            = models.CharField(max_length=150)
-
+    def __str__(self) -> str:
+        return self.name
 
 class Book(models.Model):
     name            = models.CharField(max_length=255)
     description     = models.TextField()
     publish_at      = models.DateField(auto_now=False, auto_now_add=False)
+    poster          = models.ImageField(default="books/images/default.png",upload_to=imagesave, null=True)
+    price           = models.DecimalField(max_digits=5, decimal_places=0, default=0)
+
+    category        = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    author          = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from django.urls import reverse
+from django.http import HttpResponseNotFound
 
 import json
 from .models import *
@@ -9,20 +9,39 @@ from django.contrib import messages
 
 def index(request):
     if request.method == 'POST':
-        book_form = BookForm(json.loads(request.body))
+        print(request.POST)
+        book_form = BookForm(request.POST, request.FILES)
         
         if book_form.is_valid():
             book_form = book_form.save()
             messages.success(request, message=f'Book {book_form.name} Added Successfully',extra_tags='success')
             return redirect(to='/')
+        print(book_form.errors.as_json())
         return HttpResponse(book_form.errors.as_json(), status = 400, content_type='application/json')
-    return render(request, 'home/index.html', {'books': Book.objects.all()})
+    
+    return render(request, 'home/index.html', {
+        'books': Book.objects.all(), 
+        'categories': Category.objects.all(), 
+        'authors': Author.objects.all()
+        })
+
+
+def details(request, id):
+    try:
+        return render(request, 'home/details.html', 
+            {
+                'book':Book.objects.get(id=id), 
+                'books':Book.objects.exclude(id=id).order_by('price').all()[0:5]
+            }
+        )
+    except:
+        return HttpResponseNotFound('<h1 style="text-align:center;margin-top:40px">This Book is Not Found</h1>')
 
 
 def update(request, id):
     if request.method == 'POST':
         book = Book.objects.get(id=id)
-        book_form = BookForm(json.loads(request.body), instance=book)
+        book_form = BookForm(request.POST, request.FILES, instance=book)
         if book_form.is_valid():
             book_form = book_form.save()
             messages.success(request, message=f'Book {book_form.name} updated Successfully',extra_tags='success')
@@ -40,5 +59,3 @@ def delete(request, id):
     return redirect('/')
 
 
-def details(request, id):
-    return render(request, 'home/index.html', {})
